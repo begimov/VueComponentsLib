73fd195db262ad35a7a3865e0f17811c7b1ee36f
begimov <begimov@gmail.com>
2018-08-04 10:23:27 +0300

    InputCmp basic template created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,32 @@
 <template>
   <div >
-    <button-cmp :to="{ name: 'page' }" />
-    <button-cmp :to="{ name: 'page' }" theme="success" />
-    <button-cmp :to="{ name: 'page' }" theme="success" size="large" />
+    <p>
+      <button-cmp :to="{ name: 'page' }" />
+      <button-cmp :to="{ name: 'page' }" theme="success" />
+      <button-cmp :to="{ name: 'page' }" theme="success" size="large" />
 
-    <button-cmp @click.prevent="clicked"/>
-    <button-cmp disabled />
+      <button-cmp @click.prevent="clicked"/>
+      <button-cmp disabled />
 
-    <button-cmp>PAGE</button-cmp>
+      <button-cmp>PAGE</button-cmp>
+    </p>
+
+    <p>
+      <input-cmp />
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ButtonCmp from '@/components/basic/ButtonCmp'
+import InputCmp from '@/components/basic/InputCmp'
 
 export default {
   name: 'home',
   components: {
-    ButtonCmp
+    ButtonCmp,
+    InputCmp
   },
   methods: {
     clicked() {
