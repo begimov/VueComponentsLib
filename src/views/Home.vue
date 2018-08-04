379770f3c303fd0cbfbf30290007b8ae2ad75b2f
begimov <begimov@gmail.com>
2018-08-04 07:57:44 +0300

    ButtonCmp created and imported

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div >
-    //
+    <button-cmp />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ButtonCmp from '@/components/basic/ButtonCmp'
 
 export default {
   name: 'home',
   components: {
-    //
+    ButtonCmp
   }
 }
 </script>
