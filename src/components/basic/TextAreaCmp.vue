dcac84533c63485f67d3125eaccc55a88ea23d63
begimov <begimov@gmail.com>
2018-08-05 14:43:32 +0300

    basic TextAreaCmp created

---
+++ src/components/basic/TextAreaCmp.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <input
+            v-bind="$attrs" 
+            :name="name"
+            :id="name"
+            :value="value"
+            class="form-control"
+            @input="changed"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        'value': {
+            required: false,
+            type: String,
+            default: ''
+        },
+        'name': {
+            required: true,
+            type: String
+        }
+    },
+    methods: {
+        changed (e) {
+            this.$emit('input', e.target.value)
+        }
+    },
+    mounted () {
+        //
+    }
+}
+</script>
+
